3b191a9629dd3c5510ea6d8a685a031218030ac2
Patricio Mena <luispatricio.mh@gmail.com>
2024-09-30 17:57:39 -0600

    updated select item text size

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -104,7 +104,7 @@ const SelectItem = React.forwardRef<
   <SelectPrimitive.Item
     ref={ref}
     className={cn(
-      'relative flex w-full cursor-default select-none items-center rounded-sm py-2 pl-8 pr-8 text-lg text-[#827D7D] outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+      'relative flex w-full cursor-default select-none items-center rounded-sm py-2 pl-8 pr-8 text-base sm:text-lg text-[#827D7D] outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
       className
     )}
     {...props}>
