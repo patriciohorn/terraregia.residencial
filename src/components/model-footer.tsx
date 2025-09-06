a9e17dabd4af2fd7ba6e5521c79481189533d6bb
Patricio Mena <luispatricio.mh@gmail.com>
2025-09-05 20:39:19 -0600

    Remove unused imports on footer

--- src/components/model-footer.tsx
+++ src/components/model-footer.tsx
@@ -10,10 +10,14 @@ export function ModelFooter({ items, brochure }: ModelFooterProps) {
   return (
     <div className="mt-8 flex flex-wrap items-center w-full justify-center  sm:justify-center  gap-y-6 gap-x-8 sm:max-w-md sm:mx-auto">
       <div className="flex items-center gap-x-8 justify-between">
-        {items.map((item: string) => (
+        {items?.map((item: string) => (
           <div className="flex flex-col gap-1 items-center">
-            <h3 className="text-xs text-center sm:text-base font-medium">{item.split(':')[0]}</h3>
-            <p className="text-base text-neutral-600">{item.split(':')[1]}</p>
+            <h3 className="text-xs text-center sm:text-base font-medium">
+              {item.split(':')[0]}
+            </h3>
+            <p className="text-base text-neutral-600">
+              {item.split(':')[1]}
+            </p>
           </div>
         ))}
       </div>
